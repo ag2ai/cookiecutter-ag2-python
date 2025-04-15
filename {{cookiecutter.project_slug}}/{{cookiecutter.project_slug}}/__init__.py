6d008af1edf2e3362c1fd52737b607d8fc78f775
Davor Runje <zlikowski@gmail.com>
2025-04-15 15:41:06 +0200

    Updates

--- {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/__init__.py
+++ {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/__init__.py
@@ -1,3 +1,9 @@
+# Copyright (c) 2023 - 2025, AG2ai, Inc., AG2ai open-source projects maintainers and core contributors
+#
+# SPDX-License-Identifier: Apache-2.0
+
+"""{{cookiecutter.project_description}}"""
+
 from .__about__ import __version__
 
 __all__ = ["__version__"]
