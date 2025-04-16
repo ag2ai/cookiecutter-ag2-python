53f4fdf6b271572ae7db404aca105c3e09551544
Davor Runje <zlikowski@gmail.com>
2025-04-16 04:09:51 +0200

    wip

---
+++ {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/base.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2023 - 2025, AG2ai, Inc., AG2ai open-source projects maintainers and core contributors
+#
+# SPDX-License-Identifier: Apache-2.0
+
+"""{{cookiecutter.project_description}}"""
+
+def the_meaning_of_life() -> int:
+    """Returns the meaning of life."""
+    return 42
